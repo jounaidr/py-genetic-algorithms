f4aa7a5ce20e2043db39a60afed5d31e44eed29b
jounaidr <jounaidruhomaun@googlemail.com>
2021-02-24 18:56:15 +0000

    six-hump-camel.py implemented

--- ga-continuous-distrib/six-hump-camel.py
+++ ga-continuous-distrib/six-hump-camel.py
@@ -8,24 +8,28 @@ import time
 THREADS = 5 # The amount of threads that will run the EA loop concurrently on the same population
 print_lock = Lock() # Thread lock for the print statements
 
-POPULATION_SIZE = 10 # The maximum size of the population for each generation
+POPULATION_SIZE = 100 # The maximum size of the population for each generation
 
-LOWER_BOUND = -10 # The lower limit that a gene value can be, default = -10
-UPPER_BOUND = 10 # The upper limit that a gene value can be, default = 10
+LOWER_BOUND = [-3, -2] # The lower limit that [x1, x2] gene values can be, default = [-3, -2] OR -3
+UPPER_BOUND = [3, 2] # The upper limit that [x1, x2] gene values can be, default = [3, 2] OR 2
+
+TARGET = -1.0316
 
 CROSSOVER_RATE = 0.8 # The proportion of the population that will crossover to produce offspring each generation
 MUTATION_RATE = 0.2 # The chance each offspring has of a gene (or multiple genes) being mutated each generation
-MUTATIONS = 1 # The number of genes that are mutated if an offspring is selected for mutation
+MUTATIONS = 2 # The number of genes that are mutated if an offspring is selected for mutation, MUST BE 2 IF USING SEPARATE [x1, x2] BOUNDS!
 
-GENERATIONS = 100 # The number of generations to run (if using as termination condition)
+GENERATIONS = 1000 # The number of generations to run (if using as termination condition)
 SOLUTION_FOUND = False # Whether an exact solution has been found (if using as termination condition)
 
 
-def matyas_compute_fitness(population):
-    # Calculate the result based on: 0.26*(x1^2 + x2^2) - 0.48*x1*x2 , for each individuals values in the population
-    result = (0.26 * ((population[:,0] ** 2) + (population[:,1] ** 2))) \
-             - (0.48 * population[:,0] * population[:,1])
-    fitness = abs(result[0:,] - 0) # Calculate the results absolute distance from 0, the minimal solution
+def camel_compute_fitness(population):
+    # Calculate the result based on: ((4-2.1*x1^2+(x1^4)/3) * x1^2) + (x1*x2) + ((-4+4*x2^2) * x2^2), for each individuals values in the population
+    result = ((4 - ((2.1 * population[:,0]) ** 2) + ((population[:,0] ** 4)/3)) * (population[:,0] ** 2)) \
+             + (population[:,0] * population[:,1]) \
+             + (-4 + ((4 * population[:,1]) ** 2) * (population[:,1] ** 2))
+
+    fitness = abs(result[0:,] - TARGET) # Calculate the results absolute distance from TARGET, the minimal solution
 
     return fitness
 
@@ -47,7 +51,7 @@ def main_threaded_loop(population, thread_no):
 
     # Calculate the fitness of the initial population and store fittest individual and mean fitness value data
     # NOTE: the following code can be commented out if data collection is not required
-    initial_fitness = matyas_compute_fitness(population)
+    initial_fitness = camel_compute_fitness(population)
     thread_data[1].append(initial_fitness[np.argmin(initial_fitness)])
     thread_data[2].append(np.mean(initial_fitness))
 
@@ -65,7 +69,7 @@ def main_threaded_loop(population, thread_no):
         # Choose parents from the initial population based on roulette wheel probability selection
         # Will select amount of parents to satisfy the 'CROSSOVER_RATE'
         # If 'multi_selection' set to false, parents can only be chosen once each
-        parents = selection_roulette(population, matyas_compute_fitness(population), CROSSOVER_RATE, multi_selection=True)
+        parents = selection_roulette(population, camel_compute_fitness(population), CROSSOVER_RATE, multi_selection=True)
 
         # Complete crossover of parents to produce their offspring
         # 'single_point_crossover' will choose 1 random position in each parents genome to crossover at
@@ -74,19 +78,19 @@ def main_threaded_loop(population, thread_no):
         # Mutate the children using a random gene with random value with LOWER_BOUND < x < UPPER_BOUND range
         # The chance a child will be mutated is specified using 'MUTATION_RATE'
         # The amount of genes to mutate is specified using 'MUTATIONS'
-        children = uniform_mutation(children, LOWER_BOUND, UPPER_BOUND, MUTATION_RATE, MUTATIONS)
+        children = uniform_mutation(children, LOWER_BOUND, UPPER_BOUND, MUTATION_RATE, MUTATIONS) #TODO: OPTIMISE SO THAT ONLY 1 GENE IS SELECTED FOR MUTATION
         population = np.vstack((population, children)) # Add the mutated children back into the population
 
         # Calculate the next generation of the population, this is done by killing all the weakest individuals
         # until the population is reduced to 'POPULATION_SIZE'
-        population = next_generation(population, matyas_compute_fitness(population), POPULATION_SIZE)
+        population = next_generation(population, camel_compute_fitness(population), POPULATION_SIZE)
         ###############################################################################
 
         ###############################################################################
         ################################ DATA TRACKING ################################
         ###############################################################################
         # Calculate the fitness of the current gen population
-        generation_fitness = matyas_compute_fitness(population)
+        generation_fitness = camel_compute_fitness(population)
 
         # Store fittest individual and mean fitness value data
         # NOTE: this section can commented out if data collection is not required to increase optimisation
@@ -117,12 +121,12 @@ def main_threaded_loop(population, thread_no):
         print(str(thread_data[0]) + 's')
         print('')
         print('FINAL GENERATION:')
-        display_population(population, matyas_compute_fitness(population), population.shape[0])
+        display_population(population, camel_compute_fitness(population), population.shape[0])
         print('')
         print('FITTEST INDIVIDUAL:')
         print('')
         print('#############################')
-        display_fittest_individual(population, matyas_compute_fitness(population))
+        display_fittest_individual(population, camel_compute_fitness(population))
         print('#############################')
         print('')
         print('EXECUTION TIME:')
@@ -134,7 +138,7 @@ def main_threaded_loop(population, thread_no):
 if __name__ == '__main__':
     print('')
     print('#######################################################################################')
-    print('########################### MATYAS EVOLUTIONARY ALGORITHM #############################')
+    print('####################### SIX-HUMP CAMEL EVOLUTIONARY ALGORITHM #########################')
     print('#######################################################################################')
 
     # Generate initial population given parameters, function only takes 2 dimensions so individual_size hardcoded at 2
@@ -142,7 +146,7 @@ if __name__ == '__main__':
 
     print('')
     print('INITIAL POPULATION:')
-    display_population(initial_population, matyas_compute_fitness(initial_population), initial_population.shape[0])
+    display_population(initial_population, camel_compute_fitness(initial_population), initial_population.shape[0])
     print('')
     print('STARTING EVOLUTIONARY ALGORITHM THREADS...')
 
