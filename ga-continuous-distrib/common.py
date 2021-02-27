3149618d2483d42a606284b60ba1ab3972d74539
jounaidr <jounaidruhomaun@googlemail.com>
2021-02-27 16:03:14 +0000

    Add selection tournament and selection rank methods

--- ga-continuous-distrib/common.py
+++ ga-continuous-distrib/common.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats import rankdata
 
 
 ###############################################################################
@@ -33,6 +34,47 @@ def selection_roulette(population, fitness, crossover_rate, multi_selection=True
     selection = np.random.choice(a=population.shape[0], replace=multi_selection, size=(selection_amount, 2),
                                  p=probabilities)
     parents = np.take(population, selection, axis=0)
+
+    return parents
+
+
+def selection_rank(population, fitness, crossover_rate, multi_selection=True):
+    # Rank the fitness values, then inverse rankings to get minimum rankings, then calculate probabilities from the rankings
+    rankings = (population.shape[0] + 1) - rankdata(fitness)
+    probabilities = rankings[0:, ] / np.sum(rankings)
+    # Calculate the number of parents to be selected based on crossover rate
+    selection_amount = int((population.shape[0] * crossover_rate) / 2)
+    # Select two individuals from the population given the weighted probabilities, for the amount specified by 'selection_amount'
+    selection = np.random.choice(a=population.shape[0], replace=multi_selection, size=(selection_amount, 2),
+                                 p=probabilities)
+    parents = np.take(population, selection, axis=0)
+
+    return parents
+
+
+def selection_roulette_rank(population, fitness, crossover_rate, multi_selection=True):
+    # If the standard deviation of fitness values is greater than 1, select using roulette wheel, otherwise use rank selection
+    if np.std(fitness) > 1:
+        return selection_roulette(population, fitness, crossover_rate, multi_selection)
+    else:
+        return selection_rank(population, fitness, crossover_rate, multi_selection)
+
+
+def selection_tournament(population, fitness, crossover_rate, tournament_proportion=0.3, multi_selection=True):
+    # Calculate the number of participants in the tournament based on the tournament_proportion
+    tournament_size = int(population.shape[0] * tournament_proportion)
+    # Calculate the number of parents to be selected based on crossover rate
+    selection_amount = int(population.shape[0] * crossover_rate)
+    # Combine the population with their respective fitness values
+    population_fitness = np.concatenate((population, fitness[:, None]), axis=1)
+    # Select 'tournament_size' amount of individuals from the population, for the for the amount specified by 'selection_amount'
+    tournament_selection = np.random.choice(a=population.shape[0], replace=multi_selection, size=(selection_amount, tournament_size))
+    tournaments = np.take(population_fitness, tournament_selection, axis=0)
+    # Get the winners from each tournament by using the the highest value individuals last index (fitness value) from each tournament,
+    # and then remove the last element (the previously combined fitness value)
+    winners = tournaments[np.arange(tournaments.shape[0]), np.argmax(tournaments[:, :, 10], axis=1), :-1]
+    # Format the winners so they are in the (selection_amount/2 * 2 * individual_size) parents format before returning
+    parents = np.transpose(np.asarray(np.vsplit(winners, 2)),  axes=[1, 0, 2])
 
     return parents
 
